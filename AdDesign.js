fab74570db2d0133b238e7321ec8a570549cad0e
mzhig1 <36042402+mzhig1@users.noreply.github.com>
2018-11-15 11:09:38 -0500

    Create AdDesign.js

---
+++ AdDesign.js
@@ -0,0 +1,11 @@
+fill(255, 0, 0);
+textSize(12);
+text("locks ", 150, 218);
+textSize(30);
+text("secure and cheap!", 10, 87);
+
+fill(255, 255, 255);
+ellipse(165,127,50,50);
+fill(8, 50, 166);
+rect(132,141,66,52);
+
