355917bf3dc1cabd2cb53ab17aef8be8305d196f
mzhig1 <36042402+mzhig1@users.noreply.github.com>
2018-11-15 11:13:22 -0500

    Create WhatsforDinner.js

---
+++ WhatsforDinner.js
@@ -0,0 +1,15 @@
+background(255, 255, 255); // wooden table
+fill(128, 255, 0);
+ellipse(200, 200, 350, 350); // plate
+strokeWeight(2);
+stroke(0, 0, 0);
+ellipse(200, 200, 300, 300); 
+fill(242, 255, 0);
+ellipse(200,200,50,50);
+ellipse(250,200,50,50);
+ellipse(200,250,50,50);
+ellipse(150,250,50,50);
+fill(69, 23, 40);
+rect(175,80,100,100);
+fill(112, 0, 56);
+triangle(100,100,6,200,100,250);
